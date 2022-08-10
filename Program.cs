93c87fe3d9dbfcbcbf401c109bc552bb353735e6
“Lydia <lydiajagdeo@gmail.com>
2022-08-10 10:40:29 -0400

    Operators initial

--- Program.cs
+++ Program.cs
@@ -6,13 +6,20 @@ namespace Operator
     {
         static void Main(string[] args)
         {
+            int num1 = 5;
+            int num2 = 10;
+
+            Console.WriteLine($"Add: {num1+num2}");
+            Console.WriteLine($"Subtract: {num1 - num2}");
+            Console.WriteLine($"Multiply: {num1 * num2}");
+
             int a = 17;
             int b = 4;
 
-            int divide = a / b;
+            int quotient = a / b;
 
             int remainder = a % b;
-            Console.WriteLine($"{a}/{b} is {divide} remainder {remainder}");
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
 
             Console.WriteLine("What is the radius of your circle?");
 
@@ -30,8 +37,7 @@ namespace Operator
 
             }
 
-
-
+            
 
         }
 
